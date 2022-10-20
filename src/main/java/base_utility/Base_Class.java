e271591486eb8566e1c2d5c54af32265bd7c635f
Amir <Amir@LAPTOP-0G9F9NDF>
2022-10-20 23:58:09 +0530

    Base class almost done.....thoda modification krna pdega bss..

--- src/main/java/base_utility/Base_Class.java
+++ src/main/java/base_utility/Base_Class.java
@@ -1,5 +1,50 @@
 package base_utility;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.asserts.SoftAssert;
+
+
 public class Base_Class {
+	
+	public static WebDriver driver;
+	public static SoftAssert SA;
+	public static Utility_Class util;
+	
+	public static void Initialize() {
+		SA = new SoftAssert();
+		util = new Utility_Class(driver);
+		
+	}
+	
+	public static void Launch_Browser(String Browser_Name) {
+		
+			if (Browser_Name.equalsIgnoreCase("Chrome")) {
+			
+			System.setProperty("webdriver.chrome.driver", "C:\\Java data\\Automation_Tools\\Selenium\\chromedriver_win32\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} 
+		else if(Browser_Name.equalsIgnoreCase("Edge"))
+		{
+			System.setProperty("webdriver.edge.driver", "C:\\Java data\\Automation_Tools\\Selenium\\edgedriver_win64\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		}
+//			driver.manage().window().maximize();
+			
+			driver.get("https:\\www.facebook.com/");
+	}
+	
+	public static void Quit_Browser() throws InterruptedException {
+		Thread.sleep(5000);
+		driver.quit();
+	}
+	
+	public static void Wait_Browser(int sec) throws InterruptedException {
+		Thread.sleep(sec*1000);
+	}
+
+}
+
 
 }
--- src/main/java/base_utility/Listner_Class.java
+++ src/main/java/base_utility/Listner_Class.java
@@ -2,12 +2,14 @@ package base_utility;
 	
 import java.io.IOException;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
 public class Listner_Class implements ITestListener{
-		
+		WebDriver driver;
+		Utility_Class util =  new Utility_Class(driver);
 		
 		public void onTestFailure(ITestResult result) {
 			Reporter.log("Failure Block of Code Running");
@@ -15,7 +17,11 @@ public class Listner_Class implements ITestListener{
 		
 		public void onTestSuccess(ITestResult result) {
 			Reporter.log("Success Block of Code Running");
-			
+			try {
+				util.Take_ScreenShot(result.getName());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 		public void onTestSkipped(ITestResult result) {
@@ -25,4 +31,4 @@ public class Listner_Class implements ITestListener{
 	}
 
 
-}
+

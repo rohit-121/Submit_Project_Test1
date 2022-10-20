a9b77d755d0e5ed399b463261ae57741b6689206
Amir <Amir@LAPTOP-0G9F9NDF>
2022-10-21 00:10:01 +0530

    pom class ..........LogIn Activity Started

--- src/main/java/base_utility/Listner_Class.java
+++ src/main/java/base_utility/Listner_Class.java
@@ -2,14 +2,12 @@ package base_utility;
 	
 import java.io.IOException;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 import org.testng.Reporter;
 
 public class Listner_Class implements ITestListener{
-		WebDriver driver;
-		Utility_Class util =  new Utility_Class(driver);
+		
 		
 		public void onTestFailure(ITestResult result) {
 			Reporter.log("Failure Block of Code Running");
@@ -18,7 +16,7 @@ public class Listner_Class implements ITestListener{
 		public void onTestSuccess(ITestResult result) {
 			Reporter.log("Success Block of Code Running");
 			try {
-				util.Take_ScreenShot(result.getName());
+				Base_Class.util.Take_ScreenShot(result.getName());
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
--- src/main/java/pom_package/FB_Login_Page.java
+++ src/main/java/pom_package/FB_Login_Page.java
@@ -1,5 +1,41 @@
 package pom_package;
 
-public class FB_Login_Page {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+public class FB_Login_Page {
+	
+	public FB_Login_Page(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	@FindBy (xpath = "//*[@name='email']")private WebElement UserName;
+	
+	@FindBy (xpath = "//input[@name='pass']")private WebElement Password;
+	
+	@FindBy(xpath = "//button[text()='Log in']")private WebElement ClickToLogin;
+	
+
+	
+	public void FB_Login_Page_EnterUserName(String name) throws InterruptedException {
+		Thread.sleep(2000);
+		UserName.sendKeys(name);
+	}
+	
+	public void FB_Login_Page_EnterPassword(String pass) throws InterruptedException {
+		Thread.sleep(2000);
+		Password.sendKeys(pass);
+	}
+	
+	public void FB_Login_Page_Click_LogInButton() {
+		ClickToLogin.click();
+	}
+	
+	
+	
+	
+	
+	
 }

a3520d026c2bb3c469cec3446008e11f423ab771
Amir <Amir@LAPTOP-0G9F9NDF>
2022-10-20 23:43:18 +0530

    Listener Class done

--- src/main/java/base_utility/Listner_Class.java
+++ src/main/java/base_utility/Listner_Class.java
@@ -1,5 +1,28 @@
 package base_utility;
+	
+import java.io.IOException;
 
-public class Listner_Class {
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+public class Listner_Class implements ITestListener{
+		
+		
+		public void onTestFailure(ITestResult result) {
+			Reporter.log("Failure Block of Code Running");
+		}
+		
+		public void onTestSuccess(ITestResult result) {
+			Reporter.log("Success Block of Code Running");
+			
+		}
+		
+		public void onTestSkipped(ITestResult result) {
+			Reporter.log("Skipped Block of Code Running");
+		}
+
+	}
+
 
 }
